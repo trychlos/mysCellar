b03d33bc55360cce52eecf78877da7b6ed76ffb0
Pierre Wieser <pwieser@trychlos.org>
2017-05-02 19:49:46 +0200

    Prepare next version
    Add pwiTimer class.
    let the controller configure the data rates.

--- mysCellar.ino
+++ mysCellar.ino
@@ -36,7 +36,40 @@
  * NOTE: this sketch has been written for MySensor v 2.1 library.
  * 
  * pwi 2017- 3-25 activate repeater feature
+ * pwi 2017- 4- 2 use pwiTimer + parms are updatable
  */
+
+// uncomment for debugging this sketch
+#define DEBUG_ENABLED
+
+static const char * const thisSketchName    = "mysCellar";
+static const char * const thisSketchVersion = "5.0.2017";
+
+/* 
+ * Declare the timers
+ */
+#include "pwi_timer.h"
+
+pwiTimer main_timer;
+
+pwiTimer max_frequency_timer;
+static const char   *st_max_frequency_label   = "MaxFrequencyTimer";
+static unsigned long st_max_frequency_timeout = 60000;       // 1 mn
+
+pwiTimer flood_unchanged_timer;
+static const char    *st_flood_unchanged_label  = "FloodUnchangedTimer";
+static unsigned long st_unchanged_timeout = 3600000;         // 1 h
+
+pwiTimer temp_unchanged_timer;
+static const char *st_temp_unchanged_label = "TemperatureUnchangedTimer";
+
+pwiTimer hum_unchanged_timer;
+static const char *st_hum_unchanged_label = "HumidityUnchangedTimer";
+
+pwiTimer door_unchanged_timer;
+static const char *st_door_unchanged_label = "DoorUnchangedTimer";
+
+/* The MySensors part */
 //#define MY_DEBUG
 #define MY_REPEATER_FEATURE
 #define MY_RADIO_NRF24
@@ -46,31 +79,21 @@
 #define MY_SOFT_HMAC_KEY 0xe5,0xc5,0x36,0xd8,0x4b,0x45,0x49,0x25,0xaa,0x54,0x3b,0xcc,0xf4,0xcb,0xbb,0xb7,0x77,0xa4,0x80,0xae,0x83,0x75,0x40,0xc2,0xb1,0xcb,0x72,0x50,0xaa,0x8a,0xf1,0x6d
 #include <MySensors.h>
 
-#include <DHT.h>  
-
-/*
- * Four sensors here, each one having its own:
- * - minPeriod: the minimum period, i.e. the maximum frequency, i.e. the maximum
- *   messaging rate: does not send more than one measure per 'minPeriod'
- *   (does not flood the gateway)
- * - maxPeriod: the maximum period, i.e. the minimum frequency, i.e. the minimum
- *   messaging rate: at least send one measure every 'maxPeriod'
- *   (say "I'm alive").
- *
- * Measures are done at the maximum frequency. Results are sent if a change is
- * detected, or the minimum frequency is reached.
- *
- * As a consequence, the maximum reactivity of a sensor is influenced by the
- * minimum period: after a measure has been sent at 't', any change before
- * 't+minPeriod' will be silently ignored.
- * The best reactivity is reached when a change occurs before 't+maxPeriod'.
+/* The four sensors + the main one
+ * - main let us acts on global configuration
+ * - alert sensors react to the alert timer (send at least one message if unchanged)
+ * - measure sensors react to measure timers (have a min frequency and a max frequency)
  */
 enum {
-    CHILD_ID_HUMIDITY = 0,
+    CHILD_MAIN = 0,
+    CHILD_ID_FLOOD = 10,
     CHILD_ID_TEMPERATURE,
-    CHILD_ID_FLOOD,
-    CHILD_ID_OPENING
+    CHILD_ID_HUMIDITY,
+    CHILD_ID_DOOR
 };
+
+MyMessage msgVar1( 0, V_VAR1 );
+MyMessage msgVar2( 0, V_VAR2 );
 
 /*
  * Flood detection
@@ -83,181 +106,144 @@ enum {
 #define FLOOD_DIGITALINPUT      (4)
 #define FLOOD_DIGITAL_FLOODED   (LOW)
 #define FLOOD_DIGITAL_DRY       (HIGH)
+
+bool st_flooded = false;
+
 // message type
 MyMessage msgFlood( CHILD_ID_FLOOD, V_TRIPPED );
 
+/* regarding the flood detection, we are only interested by the digital value
+ * analog value is only used during development for debugging help
+ * 
+ * Returns: %TRUE if change
+ */
+bool readFlood( void )
+{
+    bool prev_flood = st_flooded;
+    bool changed = false;
+    st_flooded = ( digitalRead( FLOOD_DIGITALINPUT ) == FLOOD_DIGITAL_FLOODED );
+    if( st_flooded != prev_flood ){
+        changed = true;
+#ifdef DEBUG_ENABLED
+        Serial.print( F( "[readFlood] flooded=" ));
+        Serial.println( st_flooded ? "True":"False" );
+#endif
+    }
+    return( changed );
+}
+
+void sendFlood( void )
+{
+    send( msgFlood.set( st_flooded ));
+    flood_unchanged_timer.restart();
+
+#ifdef DEBUG_ENABLED
+    uint8_t av = analogRead( FLOOD_ANALOGINPUT );
+    uint8_t range = map( av, FLOOD_ANALOG_MIN, FLOOD_ANALOG_MAX, 0, 3 );
+    Serial.println( F( "[RainSensor] analogValue=" ));
+    Serial.print( av );
+    Serial.print( F( ", mapped=" ));
+    Serial.print( range );
+    switch( range ){
+        case 0:    // Sensor getting wet
+            Serial.println( F( "  => Flood" ));
+            break;
+        case 1:    // Sensor getting wet
+            Serial.println( F( "  => Rain Warning" ));
+            break;
+        case 2:    // Sensor dry - To shut this up delete the " Serial.println("Not Raining"); " below.
+            Serial.println( F( "  => Not Raining" ));
+            break;
+    }
+    Serial.print( F( "[RainSensor] digitalValue=" ));
+    Serial.print( st_flooded ? "High":"Low" );
+    Serial.print( F( "  => flooded=" ));
+    Serial.println( st_flooded ? "True":"False" );
+#endif
+}
+
+void onFloodUnchangedCb( void *empty )
+{
+    sendFlood();
+    flood_unchanged_timer.restart();
+}
+
 /*
  * Temperature/Humidity DHT22/AM2302 sensors
- * Definitions
+ * As there is only one sensor for the two measures, we take the two measures
+ * together, and send the two messages together also.
  */
+#include <DHT.h>  
+DHT dht;
+
 // AM2302 DAT digital output
 #define TEMPHUM_DIGITALINPUT    (3)
+
+float st_temp = 0;
+float st_temp_prev = 0;
+float st_hum = 0;
+float st_hum_prev = 0;
+
 // message type
 MyMessage msgHum(  CHILD_ID_HUMIDITY,    V_HUM );
 MyMessage msgTemp( CHILD_ID_TEMPERATURE, V_TEMP );
-DHT dht;
 
-/*
- * Opening door detection
- * We are using here the NO contact for the door opening detector:
- * - door closed : contact is closed
- * - door opened: contact is opened.
- */
-#define OPENING_INPUT           (A0)           // use analog pin as a digital input
-// message type
-MyMessage msgOpening( CHILD_ID_OPENING, V_TRIPPED );
-
-/*
- * MySensors gateway
- */
-boolean metric = true;
-
-// set to 1 to debug the read/send functions
-static const int debug_read = 0;
-static const int debug_send = 0;
-
-/*
- * Code helpers...
- *
- * Note:
- * the Arduino compiler is not able to compile a declaration as:
- *     typedef struct _sDefines sDefines;
- *     struct _sDefines { ... };
- * and then use sDefines pointer as a variable type in function declaration.
- * We are so stucked to use 'void *' pointers.
- */
-enum {
-    TYPE_BOOLEAN = 0,
-    TYPE_FLOAT
-};
-
-typedef struct {
-    int           childId;
-    unsigned long minPeriod;
-    unsigned long maxPeriod;
-    int           typeData;
-    boolean       bValue;
-    float         fValue;
-    unsigned long lastSent;
-    boolean      ( *fnRead )( void * );
-    void         ( *fnSend )( void * );
-}
-    sDefines;
-
-boolean read_float( void *p, float data, const char *msgerr )
-{
-    if( isnan( data )){
-        Serial.println( msgerr );
-        return( false );
-    }
-    boolean changed = false;
-    sDefines *pDef = ( sDefines * ) p;
-    if( data != pDef->fValue ){
-        changed = true;
-        pDef->fValue = data;
-    }
-    return( changed );
-}
-
-boolean read_boolean( void *p, boolean data )
-{
-    boolean changed = false;
-    sDefines *pDef = ( sDefines * ) p;
-    if( data != pDef->bValue ){
-        changed = true;
-        pDef->bValue = data;
-    }
-    return( changed );
-}
-
-
-void send_float( void *p, MyMessage &msg, const char *label )
-{
-    sDefines *pDef = ( sDefines * ) p;
-    send( msg.set( pDef->fValue, 1 ));
-    if( debug_send ){
-        Serial.print( label );
-        Serial.println( pDef->fValue );
-    }
-}
-
-/*
- * temperature and humidity measures actually return the last value
+/* temperature and humidity measures actually return the last value
  * cached by the library; the library takes care itself of respecting
  * the minimal sampling period
  */
-boolean read_temperature( void *p )
+void readTemp( void )
 {
-    if( debug_read ){
-        Serial.print( "read_temperature: " );
-        Serial.println( millis());
-    }
-    return( read_float( p, dht.getTemperature(), PSTR( "Failed reading temperature from DHT" )));
+      // Get temperature from DHT library
+      float temp = dht.getTemperature();
+      if( isnan( temp )){
+          Serial.println( F( "[readTemp] failed reading temperature from DHT" ));
+      } else {
+          st_temp = temp;
+      }
 }
 
-void send_temperature( void *p )
+void readHum( void )
 {
-    send_float( p, msgTemp, PSTR( "Temperature: " ));
+      // Get temperature from DHT library
+      float hum = dht.getHumidity();
+      if( isnan( hum )){
+          Serial.println( F( "[readTemp] failed reading humidity from DHT" ));
+      } else {
+          st_hum = hum;
+      }
 }
 
-boolean read_humidity( void *p )
-{
-    if( debug_read ){
-        Serial.print( "read_humidity: " );
-        Serial.println( millis());
-    }
-    return( read_float( p, dht.getHumidity(), PSTR( "Failed reading humidity from DHT" )));
-}
-
-void send_humidity( void *p )
-{
-    send_float( p, msgHum, PSTR( "Humidity: " ));
-}
-/*
- * regarding the flood detection, we are only interested by the digital value
- * analog value is only used during development for debugging help
+/* This is triggered by the max_frequency timer
+ *  Only send the measures if they have changed
  */
-boolean read_flood( void *p )
+void sendTempHumCb( void *empty )
 {
-    if( debug_read ){
-        Serial.print( "read_flood: " );
-        Serial.println( millis());
-    }
-    return( read_boolean( p, digitalRead( FLOOD_DIGITALINPUT ) == FLOOD_DIGITAL_FLOODED ));
+    sendTempHum( false );
 }
 
-void send_flood( void *p )
+void onTempHumUnchangedCb( void *empty )
 {
-    sDefines *pDef = ( sDefines * ) p;
-    int av = analogRead( FLOOD_ANALOGINPUT );
-    int range = map( av, FLOOD_ANALOG_MIN, FLOOD_ANALOG_MAX, 0, 3 );
-    send( msgFlood.set(( uint8_t ) pDef->bValue ));
-    if( debug_send ){
-        Serial.println( "RainSensor:" );
-        Serial.print( "  analogValue=" );
-        Serial.println( av );
-        Serial.print( "  mapped=" );
-        Serial.println( range );
-        switch( range ){
-            case 0:    // Sensor getting wet
-                Serial.println("  => Flood");
-                break;
-            case 1:    // Sensor getting wet
-                Serial.println("  => Rain Warning");
-                break;
-            case 2:    // Sensor dry - To shut this up delete the " Serial.println("Not Raining"); " below.
-                Serial.println("  => Not Raining");
-                break;
-        }
-        Serial.print( "  digitalValue=" );
-        Serial.println( pDef->bValue ? "HIGH":"LOW" );
-        Serial.print( "  => Flooded=" );
-        Serial.println( pDef->bValue ? "True":"False" );
+    sendTempHum( true );
+}
+
+void sendTempHum( bool force )
+{
+    if( force || st_temp != st_temp_prev ){
+        send( msgTemp.set( st_temp, 1 ));
+        st_temp_prev = st_temp;
+        temp_unchanged_timer.restart();
     }
+    if( force || st_hum != st_hum_prev ){
+        send( msgHum.set( st_hum, 1 ));
+        st_hum_prev = st_hum;
+        hum_unchanged_timer.restart();
+    }
+    max_frequency_timer.restart();
 }
 
 /*
- * opening door
+ * Opening door detection
  * The sensor is provided by the Telemecanique door switch.
  * This is a double-relais platine, with a NO and a NC contacts.
  * The NC contact is used by origin for lights.
@@ -265,77 +251,160 @@ void send_flood( void *p )
  * - door closed : contact is closed
  * - door opened: contact is opened.
  */
-boolean read_opening( void *p )
+#define OPENING_INPUT           (A0)           // use analog pin as a digital input
+// message type
+
+bool st_opened = false;
+
+MyMessage msgDoor( CHILD_ID_DOOR, V_TRIPPED );
+
+bool readDoor( void  )
 {
-    if( debug_read ){
-        Serial.print( "read_opening: " );
-        Serial.println( millis());
+    bool prev_opened = st_opened;
+    bool changed = false;
+    st_opened = ( digitalRead( OPENING_INPUT ) == HIGH );
+    if( st_opened != prev_opened ){
+        changed = true;
+#ifdef DEBUG_ENABLED
+        Serial.print( F( "[readDoor] opened=" ));
+        Serial.println( st_opened ? "True":"False" );
+#endif
     }
-    return( read_boolean( p, digitalRead( OPENING_INPUT ) == HIGH ));
+    return( changed );
 }
 
-void send_opening( void *p )
+void sendDoor( void )
 {
-    sDefines *pDef = ( sDefines * ) p;
-    send( msgOpening.set(( uint8_t ) pDef->bValue ));
-    if( debug_send ){
-        Serial.println( PSTR( "Opening detection:" ));
-        Serial.println( pDef->bValue ? PSTR( "  Door is opened" ) : PSTR( "  Door is closed" ));
-    }
+    send( msgDoor.set( st_opened ));
+    door_unchanged_timer.restart();
+    
+#ifdef DEBUG_ENABLED
+    Serial.print( F( "[sendDoor] " ));
+    Serial.println( st_opened ? PSTR( "door is opened" ) : PSTR( "door is closed" ));
+#endif
 }
 
-sDefines aDefines[] = {
-    { CHILD_ID_TEMPERATURE, 5000, 60000, TYPE_FLOAT,   false, 0, 0, read_temperature, send_temperature },
-    { CHILD_ID_HUMIDITY,    5000, 60000, TYPE_FLOAT,   false, 0, 0, read_humidity,    send_humidity },
-    { CHILD_ID_FLOOD,      10000, 60000, TYPE_BOOLEAN, false, 0, 0, read_flood,       send_flood },
-    { CHILD_ID_OPENING,      500, 60000, TYPE_BOOLEAN, false, 0, 0, read_opening,     send_opening },
-    { -1 },
-};
+void onDoorUnchangedCb( void *empty )
+{
+    sendDoor();
+    door_unchanged_timer.restart();
+}
 
+/* **************************************************************************************
+ *  MAIN CODE
+ */
 void presentation()
 {
-    // Send the Sketch Version Information to the Gateway
-    sendSketchInfo( "mysCellar", "4.2017" );
-
-    // Register all sensors to gw (they will be created as child devices)
-    present( CHILD_ID_HUMIDITY,    S_HUM,        "Humidity measure" );
-    present( CHILD_ID_TEMPERATURE, S_TEMP,       "Temperature measure" );
+#ifdef DEBUG_ENABLED
+    Serial.println( F( "[presentation]" ));
+#endif
+    sendSketchInfo( thisSketchName, thisSketchVersion );
+    present( CHILD_MAIN, S_CUSTOM );
     present( CHILD_ID_FLOOD,       S_WATER_LEAK, "Flood detection" );
-    present( CHILD_ID_OPENING,     S_DOOR,       "Opening detection" );
+    present( CHILD_ID_TEMPERATURE, S_TEMP,       "Temperature measure" );
+    present( CHILD_ID_HUMIDITY,    S_HUM,        "Humidity measure" );
+    present( CHILD_ID_DOOR,        S_DOOR,       "Door opening detection" );
 }
 
 void setup()  
 {
-    dht.setup( TEMPHUM_DIGITALINPUT, DHT::AM2302 ); 
+#ifdef DEBUG_ENABLED
+    Serial.begin( 115200 );
+    Serial.println( F( "[setup]" ));
+#endif
 
-    // useless in my own case
-    //metric = gw.getConfig().isMetric;
-    
+    dht.setup( TEMPHUM_DIGITALINPUT, DHT::AM2302 ); 
     // setup an analog pin as a digital input
     pinMode( OPENING_INPUT, INPUT );
+
+    main_timer.start( "", 250, false, MainLoopCb, NULL );
+    main_timer.setDebug( false );
+    
+    max_frequency_timer.start( st_max_frequency_label, st_max_frequency_timeout, false, sendTempHumCb, NULL );
+    flood_unchanged_timer.start( st_flood_unchanged_label, st_unchanged_timeout, true, onFloodUnchangedCb, NULL );
+    temp_unchanged_timer.start( st_temp_unchanged_label, st_unchanged_timeout, true, onTempHumUnchangedCb, NULL );
+    hum_unchanged_timer.start( st_hum_unchanged_label, st_unchanged_timeout, true, onTempHumUnchangedCb, NULL );
+    door_unchanged_timer.start( st_door_unchanged_label, st_unchanged_timeout, true, onDoorUnchangedCb, NULL );
+    
+    pwiTimer::Dump();
 }
 
-/*
- */
-void loop()      
+void loop()
 {
-    unsigned long thisLoop = millis();
-    unsigned long thisDelay;
-    boolean changed;
+    pwiTimer::Loop();
+}
+
+/* **************************************************************************************
+ *  MainLoopCb
+ *  Is called on each timeout of the main_timer (setup to 250ms)
+ *  Take the measure, but do not send
+ */
+void MainLoopCb( void *empty )
+{
+    // send measures on max_frequency timeout only
+    readTemp();
+    readHum();
     
-    //Serial.print( "thisLoop=" );
-    //Serial.println( thisLoop );
-    for( int i=0 ; aDefines[i].childId >= 0 ; ++i ){
-        thisDelay = thisLoop - aDefines[i].lastSent;
-        if( thisDelay > aDefines[i].minPeriod ){
-            changed = aDefines[i].fnRead( &aDefines[i] );
-            if( changed || thisDelay > aDefines[i].maxPeriod ){
-                aDefines[i].lastSent = thisLoop;
-                aDefines[i].fnSend( &aDefines[i] );
-            }
+    // send alerts as soon as they are detected
+    if( readFlood()){
+        sendFlood();
+    }
+    if( readDoor()){
+        sendDoor();
+    }
+}
+
+/* **************************************************************************************
+ *  receive
+ */
+void receive(const MyMessage &message)
+{
+    char payload[2*MAX_PAYLOAD+1];
+    uint8_t cmd = message.getCommand();
+    uint8_t req;
+
+    memset( payload, '\0', sizeof( payload ));
+    message.getString( payload );
+#ifdef HAVE_DEBUG
+    Serial.print( F( "[receive] payload='" )); Serial.print( payload ); Serial.println( "'" ); 
+#endif
+
+    if( message.sensor == CHILD_MAIN ){
+        switch( cmd ){
+            case C_SET:
+                req = strlen( payload ) > 0 ? atoi( payload ) : 0;
+                switch( req ){
+                    case 1:
+                        st_max_frequency_timeout = strlen( payload ) > 2 ? atol( payload+2 ) : 0;
+                        max_frequency_timer.set( st_max_frequency_timeout );
+                        break;
+                    case 2:
+                        st_unchanged_timeout = strlen( payload ) > 2 ? atol( payload+2 ) : 0;
+                        flood_unchanged_timer.set( st_unchanged_timeout );
+                        temp_unchanged_timer.set( st_unchanged_timeout );
+                        hum_unchanged_timer.set( st_unchanged_timeout );
+                        door_unchanged_timer.set( st_unchanged_timeout );
+                        break;
+                }
+                break;
+            case C_REQ:
+                req = strlen( payload ) > 0 ? atoi( payload ) : 0;
+                switch( req ){
+                    case 1:
+                        dumpConfiguration();
+                        break;
+                }
+                break;
         }
     }
+}
 
-    wait( 100 );
+void dumpConfiguration( void )
+{
+    send( msgVar1.setSensor( CHILD_MAIN ).set( st_max_frequency_timeout ));
+    send( msgVar2.setSensor( CHILD_MAIN ).set( st_unchanged_timeout ));
+    sendFlood();
+    sendTempHum( true );
+    sendDoor();
 }
 
