1554b74d13379a6bdc8dca2a24f5391aa0aba3fd
Pierre Wieser <pwieser@trychlos.org>
2019-09-08 16:17:04 +0200

    Eeprom: rename variables to min_period/max_period

--- eeprom.cpp
+++ eeprom.cpp
@@ -1,10 +1,10 @@
 #include "eeprom.h"
-#include <untilNow.h>
 
 /* **************************************************************************************
  *  EEPROM management
  *  
  * pwi 2019- 6- 1 v1 creation
+ * pwi 2019- 9- 6 rename structure members
  */
 
 // uncomment for debugging eeprom functions
@@ -16,25 +16,25 @@
 void eepromDump( sEeprom &data )
 {
 #ifdef EEPROM_DEBUG
-    Serial.print( F( "[eepromDump] mark='" ));                       Serial.print( data.mark ); Serial.println( F( "'" ));
-    Serial.print( F( "[eepromDump] version=" ));                     Serial.println( data.version );
-    Serial.print( F( "[eepromDump] flood_armed=" ));                 Serial.println( data.flood_armed ? "True" : "False" );
-    Serial.print( F( "[eepromDump] flood_max_frequency_timeout=" )); Serial.println( data.flood_max_frequency_timeout );
-    Serial.print( F( "[eepromDump] flood_unchanged_timeout=" ));     Serial.println( data.flood_unchanged_timeout );
-    Serial.print( F( "[eepromDump] flood_grace_delay=" ));           Serial.println( data.flood_grace_delay );
-    Serial.print( F( "[eepromDump] flood_advert_period=" ));         Serial.println( data.flood_advert_period );
-    Serial.print( F( "[eepromDump] rain_max_frequency_timeout=" ));  Serial.println( data.rain_max_frequency_timeout );
-    Serial.print( F( "[eepromDump] rain_unchanged_timeout=" ));      Serial.println( data.rain_unchanged_timeout );
-    Serial.print( F( "[eepromDump] temp_max_frequency_timeout=" ));  Serial.println( data.temp_max_frequency_timeout );
-    Serial.print( F( "[eepromDump] temp_unchanged_timeout=" ));      Serial.println( data.temp_unchanged_timeout );
-    Serial.print( F( "[eepromDump] hum_max_frequency_timeout=" ));   Serial.println( data.hum_max_frequency_timeout );
-    Serial.print( F( "[eepromDump] hum_unchanged_timeout=" ));       Serial.println( data.hum_unchanged_timeout );
-    Serial.print( F( "[eepromDump] door_armed=" ));                  Serial.println( data.door_armed ? "True" : "False" );
-    Serial.print( F( "[eepromDump] door_max_frequency_timeout=" ));  Serial.println( data.door_max_frequency_timeout );
-    Serial.print( F( "[eepromDump] door_unchanged_timeout=" ));      Serial.println( data.door_unchanged_timeout );
-    Serial.print( F( "[eepromDump] door_grace_delay=" ));            Serial.println( data.door_grace_delay );
-    Serial.print( F( "[eepromDump] door_advert_period=" ));          Serial.println( data.door_advert_period );
-    Serial.print( F( "[eepromDump] auto_dump_timeout=" ));           Serial.println( data.auto_dump_timeout );
+    Serial.print( F( "[eepromDump] mark='" ));               Serial.print( data.mark ); Serial.println( F( "'" ));
+    Serial.print( F( "[eepromDump] version=" ));             Serial.println( data.version );
+    Serial.print( F( "[eepromDump] flood_armed=" ));         Serial.println( data.flood_armed ? "True" : "False" );
+    Serial.print( F( "[eepromDump] flood_min_period=" ));    Serial.println( data.flood_min_period );
+    Serial.print( F( "[eepromDump] flood_max_period=" ));    Serial.println( data.flood_max_period );
+    Serial.print( F( "[eepromDump] flood_grace_delay=" ));   Serial.println( data.flood_grace_delay );
+    Serial.print( F( "[eepromDump] flood_advert_period=" )); Serial.println( data.flood_advert_period );
+    Serial.print( F( "[eepromDump] rain_min_period=" ));     Serial.println( data.rain_min_period );
+    Serial.print( F( "[eepromDump] rain_max_period=" ));     Serial.println( data.rain_max_period );
+    Serial.print( F( "[eepromDump] temp_min_period=" ));     Serial.println( data.temp_min_period );
+    Serial.print( F( "[eepromDump] temp_max_period=" ));     Serial.println( data.temp_max_period );
+    Serial.print( F( "[eepromDump] hum_min_period=" ));      Serial.println( data.hum_min_period );
+    Serial.print( F( "[eepromDump] hum_max_period=" ));      Serial.println( data.hum_max_period );
+    Serial.print( F( "[eepromDump] door_armed=" ));          Serial.println( data.door_armed ? "True" : "False" );
+    Serial.print( F( "[eepromDump] door_min_period=" ));     Serial.println( data.door_min_period );
+    Serial.print( F( "[eepromDump] door_max_period=" ));     Serial.println( data.door_max_period );
+    Serial.print( F( "[eepromDump] door_grace_delay=" ));    Serial.println( data.door_grace_delay );
+    Serial.print( F( "[eepromDump] door_advert_period=" ));  Serial.println( data.door_advert_period );
+    Serial.print( F( "[eepromDump] auto_dump_timeout=" ));   Serial.println( data.auto_dump_timeout );
 #endif
 }
 
@@ -57,8 +57,8 @@ void eepromRead( sEeprom &data, pEepromRead pfnRead, pEepromWrite pfnWrite )
  */
 void eepromReset( sEeprom &data, pEepromWrite pfnWrite )
 {
-    unsigned long def_max_frequency_timeout = 120000;        // 2 mn
-    unsigned long def_unchanged_timeout = 3600000;           // 1 h
+    unsigned long def_min_period = 120000;        // 2 mn
+    unsigned long def_max_period = 3600000;           // 1 h
 #ifdef EEPROM_DEBUG
     Serial.println( F( "[eepromReset]" ));
 #endif
@@ -67,23 +67,23 @@ void eepromReset( sEeprom &data, pEepromWrite pfnWrite )
     data.version = EEPROM_VERSION;
   
     data.flood_armed = true;
-    data.flood_max_frequency_timeout = def_max_frequency_timeout;
-    data.flood_unchanged_timeout = def_unchanged_timeout;
+    data.flood_min_period = def_min_period;
+    data.flood_max_period = def_max_period;
     data.flood_grace_delay = 0;
     data.flood_advert_period = 0;
   
-    data.rain_max_frequency_timeout = def_max_frequency_timeout;
-    data.rain_unchanged_timeout = def_unchanged_timeout;
+    data.rain_min_period = def_min_period;
+    data.rain_max_period = def_max_period;
   
-    data.temp_max_frequency_timeout = def_max_frequency_timeout;
-    data.temp_unchanged_timeout = def_unchanged_timeout;
+    data.temp_min_period = def_min_period;
+    data.temp_max_period = def_max_period;
   
-    data.hum_max_frequency_timeout = def_max_frequency_timeout;
-    data.hum_unchanged_timeout = def_unchanged_timeout;
+    data.hum_min_period = def_min_period;
+    data.hum_max_period = def_max_period;
   
     data.door_armed = true;
-    data.door_max_frequency_timeout = 200;
-    data.door_unchanged_timeout = def_unchanged_timeout;
+    data.door_min_period = 200;
+    data.door_max_period = def_max_period;
     data.door_grace_delay = 0;
     data.door_advert_period = 0;
   
--- eeprom.h
+++ eeprom.h
@@ -12,6 +12,7 @@
  *  - 252/4 = 63 bytes per module
  * 
  * pwi 2019- 6- 1 v1 creation
+ * pwi 2019- 9- 6 rename structure members
  */
 #define EEPROM_VERSION  1
 
@@ -26,29 +27,29 @@ typedef struct {
        also keep here the permanent status of flood alarm armed (not modified on tripped)
        also keep the last tripped status (in case of a reset) */
     uint8_t       flood_armed;
-    unsigned long flood_max_frequency_timeout;
-    unsigned long flood_unchanged_timeout;
+    unsigned long flood_min_period;
+    unsigned long flood_max_period;
     unsigned long flood_grace_delay;
     unsigned long flood_advert_period;
     /* flood measure */
-    unsigned long rain_max_frequency_timeout;
-    unsigned long rain_unchanged_timeout;
+    unsigned long rain_min_period;
+    unsigned long rain_max_period;
     /* temperature measure */
-    unsigned long temp_max_frequency_timeout;
-    unsigned long temp_unchanged_timeout;
+    unsigned long temp_min_period;
+    unsigned long temp_max_period;
     /* humidity measure */
-    unsigned long hum_max_frequency_timeout;
-    unsigned long hum_unchanged_timeout;
+    unsigned long hum_min_period;
+    unsigned long hum_max_period;
     /* door opening detection */
     uint8_t       door_armed;
-    unsigned long door_max_frequency_timeout;
-    unsigned long door_unchanged_timeout;
+    unsigned long door_min_period;
+    unsigned long door_max_period;
     unsigned long door_grace_delay;
     unsigned long door_advert_period;
     /* autosend the full configuration */
     unsigned long auto_dump_timeout;
 }
-sEeprom;
+ sEeprom;
 
 void eepromDump( sEeprom &data );
 void eepromRead( sEeprom &data, pEepromRead pfnRead, pEepromWrite pfnWrite );
